ae6a74ed2811e239ab56e86cfcb632af4bc9aadf
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-10-02 22:36:31 +0000

    Using conditionals for weather

---
+++ conditionals.js
@@ -0,0 +1,8 @@
+const raining = true;
+const cold = false;
+
+// raining ? console.log("Don't forget your umbrella!") : null;
+// cold ? console.log("Make sure you pick out a scarf!") : null;
+// console.log("Now you're ready to go out!");
+
+console.log(cold ? "Make sure you pick out a scarf!" : "Short sleeves are fine.");
