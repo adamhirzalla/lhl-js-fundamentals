9236716b0e685d77ded0827caac1348a262234f1
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-10-02 22:42:19 +0000

    More conditional examples

--- conditionals.js
+++ conditionals.js
@@ -5,4 +5,9 @@ const cold = false;
 // cold ? console.log("Make sure you pick out a scarf!") : null;
 // console.log("Now you're ready to go out!");
 
-console.log(cold ? "Make sure you pick out a scarf!" : "Short sleeves are fine.");
+// console.log(cold ? "Make sure you pick out a scarf!" : "Short sleeves are fine.");
+
+const isCitizen = true;
+const age = 20;
+
+console.log(isCitizen && age >10 ? "You're eligible to vote" : "Can't vote");
