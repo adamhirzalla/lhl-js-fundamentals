e67d29de64c7d89a0911ba171eb3369bda020a32
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-10-08 23:09:22 -0400

    Adding hello.js

---
+++ hello.js
@@ -0,0 +1,9 @@
+// const sayHello = function () {
+//   console.log('Hello, world');
+// }
+
+const sayHello = (name) => console.log('Hello, ' + name);
+
+sayHello('Adam1');
+sayHello('Adam2');
+sayHello('Adam3');
